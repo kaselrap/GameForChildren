324ff76ce334e44b39a5769ff4ede6f33f2a7064
ginkomix <accer96226@gmail.com>
2018-01-22 04:50:59 +0300

    логика

--- js/mainLogic.js
+++ js/mainLogic.js
@@ -17,6 +17,23 @@ class Logic {
 
         this.studyClosedimg();
         this.workClosedimg();
+        this.aboutStudyWindow();
+        this.activeRadioButton();
+        this.startTexrStudy();
+    }
+
+    startTexrStudy() {
+        Promise.resolve()
+        .then(()=>{
+            return $.getJSON('./lang/'+this.lang+'.json'); 
+        })
+        .then((val)=>{
+            
+       
+        $('.room-description').children('.type').text(val['person'+(this.study-1)].name );
+        $('.room-description').children('.cost').children('.amount').text(val['person'+(this.study-1)].fullCost ); 
+         $('.room-description').children('.hours-left').children('.count-hours').text(val['person'+(this.study-1)].fullDay ); 
+             });
     }
 
     studyClosedimg() {
@@ -39,17 +56,17 @@ class Logic {
     }
 
     contexmenuArticle(clickedClass, hiddenClass) {
-let self = this;
+        let self = this;
         $('.'+ clickedClass +'').on('contextmenu',function(e){
-             let x = e.pageX;
-                let y = e.pageY;
+            let x = e.pageX;
+            let y = e.pageY,
+                id;
             Promise.resolve()
                 .then(()=>{
-               
+
 
                 let topMain = $('.main').offset().top,
-                    leftMain = $('.main').offset().left,
-                    id;
+                    leftMain = $('.main').offset().left;
                 let heightElem = $('.about-'+ hiddenClass).outerHeight();
                 if(x+260>=leftMain+624) {
                     x=x-240;
@@ -63,16 +80,20 @@ let self = this;
                 } else {
                     id =e.target.id;
                 }
-                $.getJSON('lang/'+self.lang+'.json', function(data) {
-                    $.each(data, function(key, val) {
-                        if(key == id) {
-                            return val;
-                        } 
 
-                    });
-                });
+                return $.getJSON('./lang/'+self.lang+'.json'); 
+
             })
                 .then((val)=>{
+                $('.room .about-'+ hiddenClass).children().children('.type').text(val[id].name);
+                $('.type-full-desctription').children('.amount').text(val[id].fullCost );
+                $('.type-full-desctription').children('.amount-hours').text(val[id].fullDay );
+
+                $('.type-desctription').children('.amount').text(val[id].distanceCost );
+                $('.type-desctription').children('.amount-hours').text(val[id].distanceDay );
+
+                $('.type-description-low').children('.amount').text(val[id].yourselfCost );
+                $('.type-description-low').children('.amount-hours').text(val[id].yourselfDay );
                 $('.room .about-'+ hiddenClass).css({
                     'top':y-10,
                     'left':x-10,
@@ -89,7 +110,24 @@ let self = this;
         });
 
     }
+    aboutStudyWindow () {
 
+        $(document).on('click', '.overlay, .buttonStudy', function () {
+            $('.about-persons').css({"display":"none", 'top' : 'inherit', 'bottom' : 'inherit', 'left': 'inherit'});
+            $('.overlay').css({'display':'none'});
+            $('.about-persons').removeClass('block-after');
+            $('.about-persons').removeClass('block-before');
+        });
+    }
+    activeRadioButton() {
+        $(document).on('click', '.choose-radio-button', function () {
+            var activeBtn = $('.about-persons').find('div[data-active=1]');
+            activeBtn.children().remove();
+            activeBtn.attr('data-active', '0');
+            $(this).prepend('<div class="active-rbtn"></div>');
+            $(this).attr('data-active', 1);
+        });
+    }
 
 
 }

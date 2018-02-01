a3de6a3fab8a2e893dcda5e437e55f7aa280ecc2
kaselrap <kaselrap@gmail.com>
2018-02-02 00:28:34 +0300

    Аватарки готовы
    Подставляй только правильные класы
    Ну в принципе шаблон есть же что вставлять
    Правда названия одежды у него галимое

---
+++ avas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Avatars</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body style="background: #f7f7f7;">
+	<div class="contentPersons content-articles">
+        <div class="persons articles">
+            <div class="person article" id="person0">
+                <img src="img/avatar/ava_colo2_mxxhdpi.png" alt="" class="person-body">
+                <img src="img/avatar/Study_level1_fxxhdpi.png" alt="" class="person-clothes">
+                <img src="img/avatar/eyebrows_color1_normalxxhdpi.png" alt="" class="person-eye-brows">
+                <img src="img/avatar/eyes_color1_normalxxhdpi.png" alt="" class="person-eye">
+                <img src="img/avatar/mouth_color1_smilexxhdpi.png" alt="" class="person-mouth">
+            </div>
+            <div class="person article " id="person1" >
+                <img class="person1" src="/img/person3-girl.png" alt="">
+            </div>
+            <div class="person article " id="person2">
+                <img class="person2"  src="/img/person4-girl.png" alt="">
+            </div>
+            <div class="person article " id="person3">
+                <img class="person3"  src="/img/person5-girl.png" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>

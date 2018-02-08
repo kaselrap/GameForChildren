6dabcc3b2ecaa8c395e6b384b66d64a63d3d3dcb
ginkomix <accer96226@gmail.com>
2018-02-08 22:36:02 +0300

    change day click

--- js/profile.js
+++ js/profile.js
@@ -10,30 +10,9 @@ $(window).on('load', function() {
 
    
 });
-//$('.bgImgProfileHov').on('click',function(){
-//    $('#avatarBlock').slideDown();
-//});
-//$('.closeAvatarBlock').on('click',function(){
-//    $('#avatarBlock').slideUp();
-//});
-//$('.avatarImg img').on('click',function(){
-//    var avatarImg = $(this).attr('id');
-//
-//    $('#imgProfile').attr('src','images/'+avatarImg);
-//    $.ajax({
-//        url:'http://avatar.php',
-//        type:'POST',
-//        data:({
-//            avatar: avatarImg
-//
-//        }),
-//        dataType:'HTML',
-//
-//    });
-//    $('#avatarBlock').slideUp();
-//});
-//var clickFlag = 0;
+
 $('.num').on("click",function(){
+     $('main').off('click');
     var w =$(this).attr('id');
 
     var q = [4], size =0, sizeNow=0;
@@ -49,6 +28,13 @@ $('.num').on("click",function(){
     }
     $('<p>0</p>').appendTo('#div'+w);  
     $('#div'+w).css('display','flex');
+    setTimeout(()=>{
+       $('main').on('click',()=>{
+        $('#div'+w).css('display','none');
+             $('main').off('click');
+    }); 
+    },4);
+    
     var maxN = 24 - sizeNow+q[w];
     for(let a =1;a<=maxN;a++){
         $('<p>'+a+'</p>').appendTo('#div'+w);  
@@ -57,25 +43,18 @@ $('.num').on("click",function(){
 
 });
 $(document).on("click",'.select p',function(){
+    
     var select = 0;
     select = parseInt($(this).text());
     $('#'+$(this).parent().siblings().attr('id')).text(select); 
     $('#div'+$(this).parent().siblings().attr('id')).css('display','none');
     $('#div'+$(this).parent().siblings().attr('id')).empty();
+    $('main').off('click');
     var q = [4],sizeNow=24;
     for(let i =0;i<=4;i++)
     {
         q[i]=parseInt($("#"+i).text());
         sizeNow-=q[i];
     }
-//    $('#sizeNow').text(sizeNow);
+
 });
-//$('.researchImages img').on('click',function(){
-//if($(this).attr('id')=='researchSelected')
-//    {
-//  $('.researchImages img').removeAttr('id','researchSelected');
-//    }else{
-//    $('.researchImages img').removeAttr('id','researchSelected');
-//    $(this).attr('id','researchSelected');
-//    }
-//});
